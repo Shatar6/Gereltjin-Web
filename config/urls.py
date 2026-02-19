d0041f7e6818bd59ba3af4f3e5a21a016bed1b52
Shatar6 <156137179+Shatar6@users.noreply.github.com>
2026-02-19 00:25:26 -0700

    base fixing

--- config/urls.py
+++ config/urls.py
@@ -20,10 +20,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('', RedirectView.as_view(url='/mn/', permanent=False)),  # Redirect root to Mongolian version
 ]
 
 

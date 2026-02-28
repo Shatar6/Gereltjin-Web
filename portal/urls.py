742924c829bce328c4cf19417d8bc0535a3da49d
Shatar6 <156137179+Shatar6@users.noreply.github.com>
2026-02-28 16:15:48 -0700

    UI updating

--- portal/urls.py
+++ portal/urls.py
@@ -17,13 +17,13 @@ urlpatterns = [
     
     # EMPLOYEE PORTAL URLS
     path('portal_login/', views.portal_login, name='portal_login'),
-    path('portal/dashboard/', views.portal_dashboard, name='portal_dashboard'),
-    path('portal/apartments/', views.portal_apartments, name='portal_apartments'),
-    path('portal/machines/', views.portal_machines, name='portal_machines'),
-    path('portal/vehicles/', views.portal_vehicles, name='portal_vehicles'),
-    path('portal/quotations/', views.portal_quotations, name='portal_quotations'),
+    path('dashboard/', views.portal_dashboard, name='portal_dashboard'),
+    path('apartments/', views.portal_apartments, name='portal_apartments'),
+    path('machines/', views.portal_machines, name='portal_machines'),
+    path('vehicles/', views.portal_vehicles, name='portal_vehicles'),
+    path('quotations/', views.portal_quotations, name='portal_quotations'),
 
-    path('portal/apartments/add/', views.portal_apartment_add, name='apartments_add'),
-    path('portal/machines/add-maintenance/', views.portal_machine_add_maintenance, name='machines_add_maintenance'),
-    path('portal/vehicles/add-service/', views.portal_vehicle_add_service, name='vehicles_add_service'),
+    path('apartments/add/', views.portal_apartment_add, name='apartments_add'),
+    path('machines/add-maintenance/', views.portal_machine_add_maintenance, name='machines_add_maintenance'),
+    path('vehicles/add-service/', views.portal_vehicle_add_service, name='vehicles_add_service'),
 ]
